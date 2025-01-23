5be4920b3329a7817aafab3fadf608d7b73f855a
Miguel Mosquera <mopri38@gmail.com>
2025-01-22 23:48:58 -0500

    feature(nodejs->pregunta1): implementar función solicitada con programación funcional

---
+++ nodejs/pregunta1.js
@@ -0,0 +1,8 @@
+function multiplicarPorDos(arr) {
+    return arr.map(num => num * 2);
+}
+
+//ejemplo
+const numeros = [1, 2, 3, 4, 5];
+const resultado = multiplicarPorDos(numeros);
+console.log(resultado);
